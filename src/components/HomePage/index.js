d78e1dcccc3c4a71a95a3817bee0b26ebc7a69a0
Alex Alfadel <alexalfadel@gmail.com>
2024-03-19 20:19:28 -0700

    fetch test button removed

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -7,24 +7,10 @@ import SearchPage from "../SearchPage/index.js"
 function HomePage() {
     const dispatch = useDispatch()
 
-    const searchParams = new URLSearchParams({
-        q: 'chicken&peas&carrots'
-    })
-    // console.log(searchParams)
-    // console.log(process.env.REACT_APP_API_KEY, '----api key')
 
-    const fetchTest = () => {
-        console.log('----fetchTestClicked')
-        dispatch(getRecipes(searchParams))
-        // dispatch(getIngredients())
-    }
-
-    console.log(process.env.REACT_APP_APP_ID)
     return (
         <div>
             <h1>Recipe HomePage</h1>
-            <button onClick={fetchTest}>Test Fetch</button>
-            {/* <button onClick={fetchTest}> Test Ingredients</button> */}
             <SearchPage />
             <RecipesPage />
         </div>
